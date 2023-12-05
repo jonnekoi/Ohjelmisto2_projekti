186cbb4e48dae378309996a1baf9f0013ffa94f1
jonne <jonnekoivisto00@gmail.com>
2023-12-05 15:31:18 +0200

    commit

--- Ohjelmisto2_projekti/atc_web/db.py
+++ Ohjelmisto2_projekti/atc_web/db.py
@@ -7,7 +7,7 @@ class Database:
         self.conn = mysql.connector.connect(
             host='localhost',
             port=3306,
-            database='flight_game',
+            database='lentopeli',
             user='root',
             password='root',
             autocommit=True
